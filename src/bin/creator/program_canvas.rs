2a0911e9747939a98b97ff71c61b00dd036a1c96
Christoph Kuhmuench <kuhmuench@email.de>
2025-12-01 16:25:11 -0500

    Added Zoom via scroll wheel

--- src/bin/creator/program_canvas.rs
+++ src/bin/creator/program_canvas.rs
@@ -43,10 +43,15 @@ impl Program<Message> for CanvasProgram {
     ) -> (iced::widget::canvas::event::Status, Option<Message>) {
         // Get the absolute cursor position - return if not available
         let cursor_position = if let Some(position) = cursor.position_in(bounds) {
-            position - state.pan_zoom_state.translation
+            position - state.pan_zoom_state.translation // * state.pan_zoom_state.scale
         } else {
             return (iced::widget::canvas::event::Status::Ignored, None);
         };
+
+        let cursor_position = Point::new(
+            cursor_position.x / state.pan_zoom_state.scale,
+            cursor_position.y / state.pan_zoom_state.scale,
+        );
 
         // Step 1: first check if one of the shapes captures the event
         // t.b.d
@@ -83,6 +88,7 @@ impl Program<Message> for CanvasProgram {
     ) -> Vec<Geometry> {
         let mut frame = Frame::new(renderer, bounds.size());
         frame.translate(state.pan_zoom_state.translation);
+        frame.scale(state.pan_zoom_state.scale);
 
         draw_grid(&mut frame, bounds);
 
@@ -123,7 +129,7 @@ fn draw_grid(frame: &mut Frame, bounds: Rectangle) {
 #[derive(Debug, Clone, Copy)]
 struct PanZoomState {
     translation: Vector,
-    _scale: f32,
+    scale: f32,
     /// The point in screen coordinates where dragging started
     drag_start: Option<Point>,
 }
@@ -132,7 +138,7 @@ impl Default for PanZoomState {
     fn default() -> Self {
         Self {
             translation: Vector::new(0.0, 0.0),
-            _scale: 1.0,
+            scale: 1.0,
             drag_start: None,
         }
     }
@@ -177,35 +183,39 @@ impl PanZoomState {
                         }
                         false // Mark as not handled
                     }
+                    mouse::Event::WheelScrolled { delta } => {
+                        match delta {
+                            mouse::ScrollDelta::Lines { x, y } => {
+                                println!("Lines Scrolled - x:{} y:{}", x, y);
+                            }
+                            mouse::ScrollDelta::Pixels { x, y } => {
+                                let scale_change = if y > 0.0 { 1.1 } else { 0.9 }; // zom by 10% in or out
+                                let new_scale = (self.scale * scale_change).clamp(0.1, 10.0); // Constrain zoom levels
+                                if self.scale != new_scale {
+                                    // Zoom relative to the cursor position (if available)
+                                    // This math ensures the point under the cursor stays under the cursor after scaling/translation
+                                    let zoom_factor = new_scale / self.scale;
+                                    // Adjust translation based on cursor position
+                                    let pos_as_vec =
+                                        Vector::new(cursor_position.x, cursor_position.y);
+                                    self.translation =
+                                        pos_as_vec + (self.translation - pos_as_vec) * zoom_factor;
+                                    self.scale = new_scale;
+                                    println!(
+                                        "Pixels Scrolled - x:{} y:{} scale: {}",
+                                        x, y, self.scale
+                                    );
+                                    return true;
+                                }
+                            }
+                        }
+                        false
+                    }
                     _ => false,
                 };
             }
             _ => {}
         }
         false
-        /*
-                mouse::Event::WheelScrolled { delta } => {
-                    // Zooming
-                    let scale_change = if delta.y > 0.0 { 1.1 } else { 0.9 }; // Simple zoom factor
-                    let new_scale = (self.scale * scale_change).clamp(0.1, 10.0); // Constrain zoom levels
-
-                    if self.scale != new_scale {
-                        // Zoom relative to the cursor position (if available)
-                        // This math ensures the point under the cursor stays under the cursor after scaling/translation
-                        if let Some(cursor_position) = mouse_event.position() {
-                            let zoom_factor = new_scale / self.scale;
-                            // Adjust translation based on cursor position
-                            self.translation = cursor_position.into()
-                                + (self.translation - cursor_position.into()) * zoom_factor;
-                        }
-                        self.scale = new_scale;
-                        return true;
-                    }
-                }
-                _ => {}
-            }
-        }
-        _ => {}
-        */
     }
 }
